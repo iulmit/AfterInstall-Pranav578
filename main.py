5d0cf061405e30c22bc18c27619bb907d675d143
Pranav <76797956+Pranav578@users.noreply.github.com>
2022-02-23 11:22:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
-
+'''
 ## check system update
 os.system("sudo apt-get update")
 os.system("sudo apt-get upgrade")
 os.system("sudo apt-get dist-upgrade")
 os.system("sudo apt-get autoremove")
-os.system("sudo apt-get clean")
-
+os.system("sudo apt-get clean")'''
+'''
 ## install git
 os.system("sudo apt-get install git")
 
@@ -15,7 +15,7 @@ os.system("sudo apt-get install code")
 
 ## install pip
 os.system("sudo apt-get install python3-pip")
-
+'''
 ## install spotify 
 
 os.system("sudo apt-get install spotify-client")
